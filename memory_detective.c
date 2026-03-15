6765d014dbe93f0e7e0c2712832d38068fbdb3c9
Raimey Owens <RO50913@trnty.edu>
2026-03-15 16:10:04 +0000

    Completed it without testing yet

--- memory_detective.c
+++ memory_detective.c
@@ -24,8 +24,10 @@ void investigate_data_types() {
     // - double called 'my_double' with value 2.71828
     // - char called 'my_char' with value 'C'
     
-    
-    
+    int my_int = 42;
+    float my_float = 3.14;
+    double my_double = 2.71828;
+    char my_char = 'C';
     
     
     // TODO: For each variable, print:
@@ -38,14 +40,20 @@ void investigate_data_types() {
     //        my_int, (void*)&my_int, sizeof(my_int));
     
     
-    
+    printf("int: value = %d, address = %p, size = %zu bytes\n", my_int, (void*)&my_int, sizeof(my_int));
+
+    printf("float: value = %f, address = %p, size = %zu bytes\n", my_float, (void*)&my_float, sizeof(my_float));
+
+    printf("double: value = %lf, address = %p, size = %zu bytes\n", my_double, (void*)&my_double, sizeof(my_double));
+
+    printf("char: value = %c, address = %p, size = %zu bytes\n", my_char, (void*)&my_char, sizeof(my_char));
     
 }
 
 // =============================================================================
 // PART 2: STACK ALLOCATION - Function Local Variables
 // =============================================================================
-// Local variables live on the stack - they're created when a function is called
+// Local variables live on the stack - they are created when a function is called
 // and destroyed when it returns.
 
 void investigate_stack() {
@@ -54,7 +62,9 @@ void investigate_stack() {
     // - A float called 'stack_float' with value 3.14
     // - A char called 'stack_char' with value 'A'
     
-    
+    int stack_int = 42;
+    float stack_float = 3.14;
+    char stack_char = 'A';
     
     
     printf("\n=== STACK ALLOCATION ===\n");
@@ -62,6 +72,9 @@ void investigate_stack() {
     // Use format specifier %p for addresses and appropriate specifiers for values
     // Example: printf("stack_int: address = %p, value = %d\n", (void*)&stack_int, stack_int);
     
+    printf("stack_int: address = %p, value = %d\n", (void*)&stack_int, stack_int);
+    printf("stack_float: address = %p, value = %f\n", (void*)&stack_float, stack_float);
+    printf("stack_char: address = %p, value = %c\n", (void*)&stack_char, stack_char);
     
     
 }
@@ -84,10 +97,10 @@ void nested_function_call() {
 // and have fixed memory addresses.
 
 // TODO: Declare a global integer variable called 'global_counter' and initialize it to 100
-
+int global_counter = 100;
 
 // TODO: Declare a static integer variable called 'static_value' and initialize it to 200
-
+static int static_value = 200;
 
 void investigate_data_segment() {
     printf("\n=== DATA SEGMENT (Global and Static Variables) ===\n");
@@ -95,6 +108,9 @@ void investigate_data_segment() {
     // TODO: Print the addresses and values of global_counter and static_value
     // Example: printf("global_counter: address = %p, value = %d\n", 
     //                 (void*)&global_counter, global_counter);
+
+    printf("global_counter: address = %p, value = %d\n",(void*)&global_counter, global_counter);
+    printf("static_value: address = %p, value = %d\n",(void*)&static_value, static_value);
     
     
 }
@@ -113,6 +129,7 @@ void investigate_heap() {
     // Store the returned pointer in a variable called 'heap_int'
     // Hint: int *heap_int = (int*)malloc(sizeof(int));
     
+    int *heap_int = (int*)malloc(sizeof(int));
     
     // TODO: Check if malloc succeeded (heap_int should not be NULL)
     // If successful:
@@ -124,12 +141,15 @@ void investigate_heap() {
     //                 (void*)heap_int, *heap_int, sizeof(int));
     
     
-    
-    
+
+    *heap_int = 777;
+    printf("heap_int: address = %p, value = %d, size = %zu bytes\n",(void*)heap_int, *heap_int, sizeof(int));
     
     // TODO: Free the allocated memory using free(heap_int)
     // Note: After free(), the pointer still holds the address, but the memory
-    // is no longer yours to use (we'll explore this more in Unit 3)
+    // is no longer yours to use (we will explore this more in Unit 3)
+
+    free(heap_int);
     
 }
 
